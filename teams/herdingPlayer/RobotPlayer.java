ca6510c72b2845788e3fc21e0b22142d7920c658
Asya Bergal <asyabergal@gmail.com>
2014-01-13 05:39:51 -0500

    Today, I realized we need to rewrite the tasks system really hard, but we don't have time. So instead, I quick fixed it in such a way that means that robots who are sent to build something cannot be re-tasked. Will make less awful later. Code currently creates a noisetower and then a pasture; actual code to herd cows there has yet to be implemented.

--- teams/herdingPlayer/RobotPlayer.java
+++ teams/herdingPlayer/RobotPlayer.java
@@ -15,53 +15,54 @@ public class RobotPlayer {
 	static Random randall = new Random();
 	static Direction allDirections[] = Direction.values();
 
+	static MapLocation pastrLoc;
+
 	// defining channels
 	static int pastrBeingMadeChan = 0;
-	static int makePastrChan = 100; //channel will have -1 if no pastures ordered
-	static int makePastrLocChan[] = new int[]{3,4,5, 6};
+	static int makePastrChan = 1; //channel will have 0 if no pastures ordered
+	static int makePastrLocChan[] = new int[]{2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22};
+	
+boolean taskSet = false;
+	
+	static int noisetowerBeingMadeChan = 23;
+	static int makeNoisetowerChan = 24;
+	static int makeNoisetowerLocChan[] = new int[]{25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45};
+	
+	
+	//next used channel should start with 11
 	
 	/*
 	 * Calls the run functions of the different types of robot
 	 */
 	public static void run(RobotController rcIn) {
 		try{
-			if (Clock.getRoundNum() < 100){
-			System.out.println("Calling new run and value is" + rcIn.readBroadcast(makePastrChan));
-			}
 			rc = rcIn;
 			Constants.Task task = Constants.Task.ATTACKING; // default task
 			MapLocation goal = rc.senseEnemyHQLocation(); // default goal
 			ArrayList<MapLocation> path = BugMove.generateBugPath(goal, rc.getLocation(), rc);
+			boolean taskSet = false;
+	
+			RobotData myData = new RobotData(task, goal, path, taskSet);
 
-			RobotData myData = new RobotData(task, goal, path);
-
-			// do this once to initialize channels at beginning of game
+			// initialize channels and then make first noisetower and pastr
 			if (rc.getType()==RobotType.HQ){
-				rc.broadcast(makePastrChan, -1);
+				rc.broadcast(makeNoisetowerChan, 0);
+				rc.broadcast(noisetowerBeingMadeChan, 0);
+				rc.broadcast(makePastrChan, 0);
+				rc.broadcast(pastrBeingMadeChan, 0);
 				rc.yield();
-				if (Clock.getRoundNum() < 100){
-				System.out.println("makePastr initialized to" + rc.readBroadcast(makePastrChan));
-				}
-				tryToSpawn();
+				sendMakeNearbyPastrCommand(5);
+//				sendMakeNearbyPastrCommand(5);
+				sendMakeDefensiveNoisetowerCommand();
 			}
-			// if pastr has been made, decrement appropriate channel so hq doesn't double-count
-			else if (rc.getType()==RobotType.PASTR){
-				if (Clock.getRoundNum() < 100){
-				System.out.println("pastr made" + rc.readBroadcast(makePastrChan));
-				}
-				rc.broadcast(pastrBeingMadeChan, rc.readBroadcast(pastrBeingMadeChan)-1);
-			}
+			
 			// splitting up behavior based on robot type
 			while(true){
 				if(rc.getType()==RobotType.HQ){
-					if (Clock.getRoundNum() < 100){
-					System.out.println("running HQ" + rc.readBroadcast(makePastrChan));
-					}
 					runHQ();
 				}else if(rc.getType()==RobotType.SOLDIER){
 					// to store data like task, goal, path between rounds, have runFoo return them for soldiers, pastrs, towers
-					//System.out.println("running soldier" + rc.readBroadcast(makePastrChan));
-					myData = runSoldier(myData.getTask(), myData.getPath(), myData.getGoal());
+					myData = runSoldier(myData.getTask(), myData.getPath(), myData.getGoal(), myData.getTaskSet());
 				}
 
 
@@ -74,30 +75,13 @@ public class RobotPlayer {
 	/*
 	 * Run function of the HQ - try to spawn as many robots as possible
 	 */
-	private static void runHQ() throws GameActionException {
-		//rc.setIndicatorString(0, "" + rc.readBroadcast(makePastrChan));
-		//rc.setIndicatorString(1, "" + rc.readBroadcast(pastrBeingMadeChan));
-
-		// if there are no pastures and none are being made, and none have been ordered, send order to make one
-		//if ((rc.readBroadcast(pastrBeingMadeChan) == 0) && (rc.sensePastrLocations(rc.getTeam()).length == 0) && (rc.readBroadcast(makePastrChan) == -1)){
+	private static void runHQ() throws GameActionException 
+	{
+		rc.setIndicatorString(0, "" + rc.readBroadcast(makeNoisetowerChan));
+		rc.setIndicatorString(1, "" + rc.readBroadcast(makePastrChan));
 		
-		// trying to make 2 pastrs
-		int pastrBeingMade = rc.readBroadcast(pastrBeingMadeChan);
-		int pastrAlreadyMade = rc.sensePastrLocations(rc.getTeam()).length; 
-		int pastrUnansweredCommands = rc.readBroadcast(makePastrChan) + 1; // no commands sent when -1
-		if(pastrBeingMade + pastrAlreadyMade + pastrUnansweredCommands < 2 ){
-			if (Clock.getRoundNum() < 100){
-			System.out.println("trying to make pastr and value is" + rc.readBroadcast(makePastrChan));
-			System.out.println("**Calling makeNearbyPastr");
-			}
-			sendMakeNearbyPastrCommand(5);
-			rc.yield();
-		}
-		if (Clock.getRoundNum() < 100){
-		System.out.println("trying to spawn and value is" + rc.readBroadcast(makePastrChan));
-		}
 		tryToSpawn();
-
+		
 	}
 
 
@@ -121,44 +105,64 @@ public class RobotPlayer {
 	/*
 	 * Runs soldiers - herding practice
 	 */
-	private static RobotData runSoldier(Constants.Task task, ArrayList<MapLocation> path, MapLocation goal) throws GameActionException{
+	private static RobotData runSoldier(Constants.Task task, ArrayList<MapLocation> path, MapLocation goal, boolean taskSet) throws GameActionException{
 		//TODO: Select group of cows to herd
 		//TODO: herd cows towards pastr
+		
+		
+		rc.setIndicatorString(2, "" + task);
 
-		//rc.setIndicatorString(2, "" + task);
+		int pastrsToMake = rc.readBroadcast(makePastrChan);
+		int pastrsBeingMade = rc.readBroadcast(pastrBeingMadeChan);
 
-		//if there is a command sent out to make a pastr, make one 
-		int lastPastrNum = rc.readBroadcast(makePastrChan);
-		if (lastPastrNum > -1  && task != Constants.Task.PASTRMAKING){
-			if (Clock.getRoundNum() < 100){
-			System.out.println("a robot is using makePastr at " + lastPastrNum);
-			}
-			rc.setIndicatorString(0,"makePastrChan is " + lastPastrNum);
-			goal = VectorFunctions.intToLoc(rc.readBroadcast(makePastrLocChan[lastPastrNum]));
+		int noisetowersToMake = rc.readBroadcast(makeNoisetowerChan);
+		int noisetowersBeingMade = rc.readBroadcast(noisetowerBeingMadeChan);
+
+		rc.setIndicatorString(1, "" +  pastrsToMake + " " + pastrsBeingMade + " " + noisetowersToMake + " " + noisetowersBeingMade);
+
+		//if there is a noisetower to make that isn't being made, make one
+		if(taskSet == false && noisetowersToMake > 0 && noisetowersBeingMade < noisetowersToMake){
+			goal = VectorFunctions.intToLoc(rc.readBroadcast(makeNoisetowerLocChan[noisetowersToMake]));
+			rc.setIndicatorString(1, "" + goal.x + " " + goal.y);
 			path = BugMove.generateBugPath(goal, rc.getLocation(), rc);
-			task = Constants.Task.PASTRMAKING;
+						
+			task = Constants.Task.TOWERMAKING;
 
-			rc.broadcast(pastrBeingMadeChan, rc.readBroadcast(pastrBeingMadeChan)+1);
-
-			//decrement makePastrChan so other robots don't try to make the same pastr
-			rc.broadcast(makePastrChan, lastPastrNum-1);
-			if (Clock.getRoundNum() < 100){
-			System.out.println("robot decremented makePastrChan to " + (lastPastrNum-1));
-			}
+			taskSet = true;
+			rc.broadcast(noisetowerBeingMadeChan, noisetowersBeingMade + 1);
 		}
+		
+		//if there is a pastr to make that isn't being made, make one
+		if (taskSet == false && pastrsToMake > 0 && pastrsBeingMade < pastrsToMake){
+			goal = VectorFunctions.intToLoc(rc.readBroadcast(makePastrLocChan[pastrsToMake]));
+			path = BugMove.generateBugPath(goal, rc.getLocation(), rc);
+			
+			task = Constants.Task.PASTRMAKING;
+			taskSet = true;
+			
+			rc.broadcast(pastrBeingMadeChan, pastrsBeingMade + 1);
 
-
-		//if robot is active, move or perform tasl
+		}
+		
+		//if robot is active, move or perform task
 		if (rc.isActive()){
 			//if have reached goal, perform designated task
 			if (rc.getLocation().equals(goal)){
 				//rc.setIndicatorString(1, "completing Task");
 				switch (task){
 				case PASTRMAKING:
+					rc.setIndicatorString(1,"making PASTR");
 					rc.construct(RobotType.PASTR);
+					rc.broadcast(makePastrChan, pastrsToMake-1);
+					rc.broadcast(pastrBeingMadeChan, pastrsBeingMade - 1);
+					//taskSet = false;
 					break;
 				case TOWERMAKING:
+					rc.setIndicatorString(1,"making tower");
 					rc.construct(RobotType.NOISETOWER);
+					rc.broadcast(makeNoisetowerChan, noisetowersToMake-1);
+					rc.broadcast(noisetowerBeingMadeChan, noisetowersBeingMade - 1);
+					//taskSet = false;
 					break;
 				case ATTACKING:
 					Robot[] nearbyEnemies = rc.senseNearbyGameObjects(Robot.class,10,rc.getTeam().opponent());
@@ -175,12 +179,12 @@ public class RobotPlayer {
 			// if not already at goal, move towards goal
 			// TODO: allow interruptions
 			else{
-				//rc.setIndicatorString(1, "followingPath");
+				rc.setIndicatorString(1, "followingPath");
 				BugMove.followPath(path);
 			}
 		}
 
-		return new RobotData(task, goal, path);
+		return new RobotData(task, goal, path, taskSet);
 	}
 
 
@@ -189,73 +193,48 @@ public class RobotPlayer {
 	 * @Location: place where PASTR should be made
 	 */
 	private static void sendMakeNearbyPastrCommand(int pastrDistance) throws GameActionException{
-		System.out.println("*** inside send pastr command");
 		int currentMakeCount = rc.readBroadcast(makePastrChan);
 		int hqX = rc.senseHQLocation().x;
 		int hqY = rc.senseHQLocation().y;
 		
-		MapLocation pastrLoc = new MapLocation(VectorFunctions.abs(randall.nextInt(2*pastrDistance)-pastrDistance + hqX),VectorFunctions.abs(randall.nextInt(2*pastrDistance)-pastrDistance + hqY));
+		pastrLoc = new MapLocation(VectorFunctions.abs(randall.nextInt(2*pastrDistance)-pastrDistance + hqX),VectorFunctions.abs(randall.nextInt(2*pastrDistance)-pastrDistance + hqY));
 
 		// make sure that is not an obstacle 
 		//TODO: try a few times to make sure is a field?
 		while(rc.senseTerrainTile(pastrLoc).ordinal() > 1){ //0 NORMAL, 1 ROAD, 2 VOID, 3 OFF_MAP
-			pastrLoc = new MapLocation(VectorFunctions.abs(randall.nextInt(2*pastrDistance)-pastrDistance + hqX),VectorFunctions.abs(randall.nextInt(2*pastrDistance)-pastrDistance + hqY));
-			}
-
-		//broadcast that a channel should be made, and in desired position
-		// make sure that position is broadcast before soldiers are tolstr and value is0str and value is0d to read it
-		System.out.println("******* makePastr currently At" + currentMakeCount);
-		rc.broadcast(makePastrLocChan[currentMakeCount + 1], VectorFunctions.locToInt(pastrLoc));
-		System.out.println("************posting position to Channel" + makePastrLocChan[currentMakeCount + 1]);
-
-		rc.broadcast(makePastrChan, currentMakeCount + 1);
-		System.out.println("******* makePastr incremented to" + rc.readBroadcast(makePastrChan));
-        System.out.println("***************goal is at" + pastrLoc.x + ", " + pastrLoc.y);
-		
-	}
-	
-	private static void sendMakePastrCommand(MapLocation pastrLocation) throws GameActionException{
-		int currentMakeCount = rc.readBroadcast(makePastrChan);
+			pastrLoc = new MapLocation(randall.nextInt(pastrDistance),randall.nextInt(pastrDistance));
+		}
 
 		//broadcast that a channel should be made, and in desired position
 		// make sure that position is broadcast before soldiers are told to read it
-		rc.broadcast(makePastrLocChan[currentMakeCount + 1 ], VectorFunctions.locToInt(pastrLocation));
-		System.out.println("*******Printing location to " + makePastrLocChan[currentMakeCount+1]);
+		rc.broadcast(makePastrLocChan[currentMakeCount + 1 ], VectorFunctions.locToInt(pastrLoc));
 		rc.broadcast(makePastrChan, currentMakeCount + 1);	
-		System.out.println("makePastr has been incremented");
 	}
 	
-	
-
-	/*
-	 * 	finds the best several pastr locations, 
-	 *  where 'best' is defined as the locations within a certain distance from hq that have the highest cow growth rate
-	 *  also only returns locations that are not currently occupied b
-	 * @ distanceDesired - distance radius from hq which is considered
-	 * @ numPastrs - the 'best' n locations are returned, where n = numPastrs
-	 */
-	private MapLocation[] pickCloseFertilePastr(int distanceDesired, int numPastrs) throws GameActionException{
-		   double[][] cowGrowthArray = rc.senseCowGrowth();
-		   MapLocation[] bestPastrs = new MapLocation[numPastrs];
-		   double[] bestCows[]; //TODO: fix
-		   
-		   int hqX = rc.senseHQLocation().x;
-		   int hqY = rc.senseHQLocation().y;
-		   
-		   // iterating over locations in a square of 'radius' distanceDesired centered at hq location
-		   for (int x = -1*distanceDesired + hqX; x <= distanceDesired + hqX; x++){
-			   for (int y = -1*distanceDesired + hqY; y <= distanceDesired + hqY; y++){
-				   // checking that is a valid location
-				   //TODO: check that can read a location when try to path to it?
-				   if (x > 0 && y > 0 && x < cowGrowthArray.length && y < cowGrowthArray[0].length){
-					   if (true){//(cowGrowthArray[x][y] > bestPastrs[0]){ FIX
-						   
-					   }
-				   }
-			   }
-		   }
-		   
-		   return bestPastrs;
+	//make a defensive noisetower close to the nearest constructed pasture
+	private static void sendMakeDefensiveNoisetowerCommand() throws GameActionException{
+		int currentMakeCount = rc.readBroadcast(makeNoisetowerChan);
+		
+		//choose a free location adjacent to where your pasture was created	
+		boolean foundLocation = false;
+		//TODO: This should never happen, but maybe map location shouldn't default to this anyway?
+		MapLocation noisetowerLoc = new MapLocation(0, 0);
+		for(int i = -1; i <= 1; i++){
+			for(int j = -1; j <= 1; j++){
+			noisetowerLoc = VectorFunctions.mladd(pastrLoc, new MapLocation(i, j));
+			//make sure noisetower can be placed
+			if(rc.senseTerrainTile(noisetowerLoc).ordinal() <= 1) {
+				foundLocation = true;
+				break;
+			}
+			if(foundLocation) break;
+			}
 		}
-
+	
+		//broadcast that a channel should be made, and in desired position
+		// make sure that position is broadcast before soldiers are told to read it
+		rc.broadcast(makeNoisetowerLocChan[currentMakeCount + 1], VectorFunctions.locToInt(noisetowerLoc));
+		rc.broadcast(makeNoisetowerChan, currentMakeCount + 1);
+	}
 }
+	
