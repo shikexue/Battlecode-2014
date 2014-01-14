53087788da169ef44b8047f9aa9d1a89dc5534dc
biopower <shikexue@mit.edu>
2014-01-13 22:58:21 -0500

    smarter attackign

--- teams/herdingPlayer/Constants.java
+++ teams/herdingPlayer/Constants.java
@@ -1,8 +1,8 @@
 package herdingPlayer;
 
-public final class Constants {
-	
+public final class Constants {	
 	public static enum Task {
 		HERDING, ATTACKING, DEFENDING, PASTRMAKING, TOWERMAKING
 	}
+	
 }
